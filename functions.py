250fef695e8e54293bb83b89874af603ef990b92
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-06-03 12:57:58 +0530

    Added stylish text

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import base64
+import re
 
 
 def encode(text):
@@ -9,3 +10,25 @@ def enhanceText(text):
     text = text.replace('.', '.', text.count('.')).replace(',', ', ', text.count(','))
     text = " ".join(text.split()).replace(" . ", ". ")
     return text
+
+
+def stylish_text(text):
+    text = text.lower()
+    style_text = list('𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇')
+    normal_text = list('abcdefghijklmnopqrstuvwxyz')
+
+    result = []
+
+    for char in list(text):
+        if char in normal_text:
+            result.append(style_text[normal_text.index(char)])
+        else:
+            result.append(char)
+
+    return ''.join(result)
+
+
+def checkForURLs(string):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex, string)
+    return [x[0] for x in url]
