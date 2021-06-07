8f5b767bee0a6617f6d23b19b7d606dbc4e29f63
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-06-07 22:47:46 +0530

    Emojies
    Slaps messages now contain emojies!

--- slap.py
+++ slap.py
@@ -1,23 +1,113 @@
 import random
-import re
+import textblob
 from urllib import request
+import re
+import nltk
+
+from nltk.corpus import words
+from nltk.stem import WordNetLemmatizer
+from textblob import TextBlob
 
 from info import USER_AGENT
+
+nltk.download('words')
+
+wordNetLemmatizer = WordNetLemmatizer()
+
+file = open('non-code-files/filtered-emojis.txt', 'rb')
+emoji_values = file.read().decode().splitlines()
+file.close()
+
+emojis = []
+emoji_text = []
+
+for line in emoji_values:
+    text1 = line.split(' ')
+
+    emojis.append(text1[0])
+    emoji_text.append(text1[1])
+
+
+def replaceSpaceEmojis(choice, text):
+    if ' ' in text:
+        textRes = str(text).replace(' ', random.choice(choice), 1)
+    else:
+        textRes = str(text) + ' ' + random.choice(choice)
+    return textRes
+
+
+def withEmojis(text):
+    words = text.split(" ")
+
+    result = 0
+    textRes = ''
+
+    for feedback in words:
+        feedback_polarity = TextBlob(feedback).sentiment.polarity
+
+        result = result + feedback_polarity
+
+    positiveEmojis = '😀 😃 😄 😁 ☺ ️😇 😊 🙂 🙃 😉 😌 😍 🥰 😝 🥳 🤩 😎 🤓'.split(' ')
+    moderateEmojis = '😮 😐 😑 😓 🤔 🤥 😯 🤫 😲 😮'.split(' ')
+    negativeEmojis = '😧 🥺 😢 😭 😩 😩 😮 😦 😦 😣 😖 ☹ ️ 🙁 😕 😟 😔 😞 😰 😥 😨'.split(' ')
+
+    if result > 0.5:
+        textRes = replaceSpaceEmojis(positiveEmojis, text)
+    elif result == 0:
+        textRes = replaceSpaceEmojis(moderateEmojis, text)
+    else:
+        textRes = replaceSpaceEmojis(negativeEmojis, text)
+    return textRes
+
+
+def add_emojis(text):
+    replace_target = ['-', '_', '=']
+
+    for replace in replace_target:
+        text = text.replace(replace, ' ')
+
+    text_new = []
+
+    for word in text.split(' '):
+        text_new.append(word)
+        new_word = re.sub(r"(\w)([A-Z])", r"\1 \2", word)
+
+        can_pass = word in words.words()
+
+        word = new_word
+
+        if not can_pass:
+            for segment in new_word.split(' '):
+                segment = segment.lower()
+                can_pass = segment in words.words()
+
+        if can_pass:
+            word = word.lower()
+            word_stemmed = wordNetLemmatizer.lemmatize(word)
+
+            for word1 in word_stemmed.split(' '):
+                try:
+                    i = emoji_text.index(word1)
+                    text_new.append(emojis[i])
+                except ValueError:
+                    pass
+
+    return ' '.join(text_new)
 
 
 fruits = open('non-code-files/fruits.txt', 'r').read()
 fruits = fruits.split('\n')
 slaps_texts = 'https://raw.githubusercontent.com/XomaDev/asteroid-bot-dev/main/external_files/asteroid-slaps.txt'
 
+web_request = request.Request(slaps_texts)
+web_request.add_header("User-Agent", USER_AGENT)
+
+response = request.urlopen(web_request).read().decode()
+
 
 def slap(user, to):
     user = re.sub(r'[0-9]+', '', user)
     to = re.sub(r'[0-9]+', '', to)
-
-    web_request = request.Request(slaps_texts)
-    web_request.add_header("User-Agent", USER_AGENT)
-
-    response = request.urlopen(web_request).read().decode()
 
     parts = response.split('-- ignore')
     is_first_selection = False
@@ -42,4 +132,4 @@ def slap(user, to):
     choice = choice.replace("<fruit>", random_fruit)
     choice = choice.replace("<r_num>", str(random.randint(0, 12)))
 
-    return choice
+    return withEmojis(add_emojis(choice))
