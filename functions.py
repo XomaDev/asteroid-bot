6e3a362132037e2e5f14df1e4c7afaabd49e6843
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-06-08 13:03:03 +0530

    Added function

--- functions.py
+++ functions.py
@@ -32,3 +32,14 @@ def checkForURLs(string):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, string)
     return [x[0] for x in url]
+
+
+def replace_special_slash(text):
+    characters = '!@#$%^&*()-+?_=,<>/".' + "''"
+
+    new_string = ""
+    for i in text:
+        if i in characters:
+            new_string += '\\'
+        new_string += i
+    return new_string
